c486353809fc28bd3e2e84d2e80298cb1304297e
Ayushman444 <ayushman5022@gmail.com>
2023-08-26 17:43:34 +0530

    First commit by Ayushman

---
+++ src/components/Testimonials.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+import { Card } from './Card';
+
+import {FiChevronLeft,FiChevronRight} from "react-icons/fi"
+
+export const Testimonials = (props) => {
+    let reviews = props.reviews;
+
+    const [index,setIndex]= useState(0);
+
+    function leftShiftHandler(){
+      if(index-1<0){
+        setIndex(reviews.length-1)
+      }
+      else{
+        setIndex(index-1)
+      }
+    }
+
+    function rightShiftHandler(){
+      if(index+1>=reviews.length){
+        setIndex(0);
+      }
+      else{
+        setIndex(index+1);
+      }
+    }
+
+    function surpriseHandler(){
+
+    }
+
+  return (
+    <div>
+        <Card review ={reviews[0]}/>
+
+        <div className='flex text-3xl mt-5 gap-3 text-violet-400 font-bold mx-auto'>
+            <button 
+            onClick={leftShiftHandler}
+            className='cursor-pointer hover:text-violet-500'>
+                <FiChevronLeft/>
+            </button>
+
+            <button
+            onClick={rightShiftHandler}
+            className='cursor-pointer hover:text-violet-500'>
+                <FiChevronRight/>
+            </button>
+        </div>
+
+        <div>
+            <button 
+            onClick={surpriseHandler}
+            className='bg-violet-400 hover:bg-violet-500 transition-all duration-200
+             cursor-pointer rounded-md font-bold px-10 py-2 text-white text-lg'>
+                Surprise me
+            </button>
+        </div>
+
+    </div>
+  )
+}
